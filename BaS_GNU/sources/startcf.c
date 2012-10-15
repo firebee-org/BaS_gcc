3db8bdf7c5fcc4663ede94849b181ab239c7913e
Markus Fröschle <mfro@mubf.de>
2012-10-15 10:19:45 +0000

    fixed startup code (comment at wrong position) which prevented RAMBAR0 from getting initialized

--- BaS_GNU/sources/startcf.c
+++ BaS_GNU/sources/startcf.c
@@ -36,9 +36,9 @@ void startup(void)
 
 	__asm__ __volatile__(
 		/* Initialize RAMBARs: locate SRAM and validate it */
-		"		move.l	#__RAMBAR0 + 0x7,d0\n\t | supervisor only"
+		"		move.l	#__RAMBAR0 + 0x7,d0\n\t" 	/* supervisor only */
 		"		movec	d0,RAMBAR0\n\t"
-		"		move.l	#__RAMBAR1 + 0x1,d0\n\t"""
+		"		move.l	#__RAMBAR1 + 0x1,d0\n\t"
 		"		movec	d0,RAMBAR1\n\t"
 		/* set stack pointer to end of SRAM1 */
 		"		lea 	__SUP_SP,a7\n\t"
