f13e2c9a5ac4b08a467b66c13016f5a9b367cba3
Markus Fröschle <mfro@mubf.de>
2012-10-19 06:25:00 +0000

    removed superflous semicolon

--- BaS_GNU/sources/sysinit.h
+++ BaS_GNU/sources/sysinit.h
@@ -12,7 +12,7 @@
 extern void wait_10us(void);
 
 /* send a 16-bit word out on the serial port */
-#define uart_out_word(a)	MCF_PSC0_PSCTB_8BIT = (a);
+#define uart_out_word(a)	MCF_PSC0_PSCTB_8BIT = (a)
 
 /* adresses where FPGA data lives in flash */
 #define FPGA_FLASH_DATA ((uint8_t *) 0xe0700000L)
