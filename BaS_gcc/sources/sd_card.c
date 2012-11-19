45949351ed5421ab1798d9fcd145c8d9ff1731b4
Markus Fröschle <mfro@mubf.de>
2012-11-19 06:37:17 +0000

    new include file sd_card.h

--- BaS_gcc/sources/sd_card.c
+++ BaS_gcc/sources/sd_card.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <MCF5475.h>
 #include "bas_printf.h"
+#include "sd_card.h"
 
 extern void wait(volatile uint32_t value);
 
@@ -62,15 +63,6 @@ uint32_t sd_com(uint32_t data)
 void sd_card_init(void)
 {
 	uint32_t ret;
-	const uint32_t DSPI_DMCR_CONF = MCF_DSPI_DMCR_MSTR |	/* FireBee is DSPI master*/	/* 8 bit CS5 on */
-			MCF_DSPI_DMCR_CSIS3 |	/* CS3 inactive */
-			MCF_DSPI_DMCR_CSIS2 |	/* CS2 inactive */
-			MCF_DSPI_DMCR_DTXF |	/* disable transmit FIFO */
-			MCF_DSPI_DMCR_DRXF |	/* disable receive FIFO */
-			MCF_DSPI_DMCR_CTXF |	/* clear transmit FIFO */
-			MCF_DSPI_DMCR_CRXF;		/* clear receive FIFO */
-			/* 0x800d3c00 */
-
 	int i;
 
 	xprintf("SD-Card initialization: ");
