7cdf71810fdc61eefa25742c16c16d7c53588240
Markus Fröschle <mfro@mubf.de>
2012-12-04 08:49:21 +0000

    renamed functions to spi_...() and sd_card...() according to target

--- sources/init_fpga.c
+++ sources/init_fpga.c
@@ -1,9 +1,25 @@
 /*
  * init_fpga.c
  *
+ * This file is part of BaS_gcc.
+ *
+ * BaS_gcc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BaS_gcc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BaS_gcc.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *  Created on: 16.10.2012
  *      Author: Markus Fröschle
  */
+
 #include <MCF5475.h>
 #include "sysinit.h"
 
