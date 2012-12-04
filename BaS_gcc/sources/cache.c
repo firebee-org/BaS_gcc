a53d35f9b54a7433189c5d18a3c33515634ebd4a
Markus Fröschle <mfro@mubf.de>
2012-12-04 08:49:21 +0000

    renamed functions to spi_...() and sd_card...() according to target

--- BaS_gcc/sources/cache.c
+++ BaS_gcc/sources/cache.c
@@ -1,6 +1,22 @@
 /*
  * cache handling
+ *
+ * This file is part of BaS_gcc.
+ *
+ * BaS_gcc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BaS_gcc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BaS_gcc.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "cache.h"
 
 void flush_and_invalidate_caches(void)
