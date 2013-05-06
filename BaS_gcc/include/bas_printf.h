15ee236b933b13412c99a7373bda8aa69ef813a3
Markus Fröschle <mfro@mubf.de>
2013-05-06 05:22:31 +0000

    new stddef.h from m68k-atari-mint toolchain apparently contains a conflicting typedef for size_t

--- BaS_gcc/include/bas_printf.h
+++ BaS_gcc/include/bas_printf.h
@@ -20,7 +20,7 @@
 #ifndef _BAS_PRINTF_H_
 #define _BAS_PRINTF_H_
 #include <stdarg.h>
-typedef uint32_t size_t;
+#include <stddef.h>
 
 extern void xvsnprintf(char *str, size_t size, const char *fmt, va_list va);
 extern void xvprintf(const char *fmt, va_list va);
