257f23b55c37d0f8b3b03570c5208901f4ebc83a
Markus Fröschle <mfro@mubf.de>
2013-07-14 19:34:00 +0000

    fixed formatting

--- sources/mmc.c
+++ sources/mmc.c
@@ -70,21 +70,21 @@
 #define ACMD41	(0x80+41)		/* SEND_OP_COND (SDC) */
 #define CMD8	(8)				/* SEND_IF_COND */
 #define CMD9	(9)				/* SEND_CSD */
-#define CMD10	(10)				/* SEND_CID */
-#define CMD12	(12)				/* STOP_TRANSMISSION */
+#define CMD10	(10)			/* SEND_CID */
+#define CMD12	(12)			/* STOP_TRANSMISSION */
 #define ACMD13	(0x80+13)		/* SD_STATUS (SDC) */
-#define CMD16	(16)				/* SET_BLOCKLEN */
-#define CMD17	(17)				/* READ_SINGLE_BLOCK */
-#define CMD18	(18)				/* READ_MULTIPLE_BLOCK */
-#define CMD23	(23)				/* SET_BLOCK_COUNT (MMC) */
+#define CMD16	(16)			/* SET_BLOCKLEN */
+#define CMD17	(17)			/* READ_SINGLE_BLOCK */
+#define CMD18	(18)			/* READ_MULTIPLE_BLOCK */
+#define CMD23	(23)			/* SET_BLOCK_COUNT (MMC) */
 #define ACMD23	(0x80+23)		/* SET_WR_BLK_ERASE_COUNT (SDC) */
-#define CMD24	(24)				/* WRITE_BLOCK */
-#define CMD25	(25)				/* WRITE_MULTIPLE_BLOCK */
-#define CMD32	(32)				/* ERASE_ER_BLK_START */
-#define CMD33	(33)				/* ERASE_ER_BLK_END */
-#define CMD38	(38)				/* ERASE */
-#define CMD55	(55)				/* APP_CMD */
-#define CMD58	(58)				/* READ_OCR */
+#define CMD24	(24)			/* WRITE_BLOCK */
+#define CMD25	(25)			/* WRITE_MULTIPLE_BLOCK */
+#define CMD32	(32)			/* ERASE_ER_BLK_START */
+#define CMD33	(33)			/* ERASE_ER_BLK_END */
+#define CMD38	(38)			/* ERASE */
+#define CMD55	(55)			/* APP_CMD */
+#define CMD58	(58)			/* READ_OCR */
 
 
 static volatile DSTATUS Stat = 0 /* STA_NOINIT */;	/* Physical drive status */
