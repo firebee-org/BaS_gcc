bf0945b5ff2f59e8dd796e47478662d1e406ad19
Markus Fröschle <mfro@mubf.de>
2013-07-30 19:19:14 +0000

    memcpy() with Coldfire DMA started

--- sources/spidma.c
+++ sources/spidma.c
@@ -7,10 +7,23 @@
 
 #include "spidma.h"
 #include <MCD_dma.h>
+#include "mcd_initiators.h"
 #include "bas_printf.h"
 
 extern char _SYS_SRAM[];
 #define SYS_SRAM &_SYS_SRAM[0]
+
+void *dma_memcpy(void *dst, void *src, size_t n)
+{
+	int ret;
+
+	ret = MCD_startDma(0, src, 1, dst, 1, n, 1, DMA_ALWAYS, 0, MCD_SINGLE_DMA, 0);
+	__asm__ __volatile__("move.w	sr,d0\n\t"
+						 "stop		#0x270\n\t"
+						 "move.w	d0,sr": : :);	/* halt CPU until DMA finished */
+
+	return dst;
+}
 
 int spidma_init(void)
 {
