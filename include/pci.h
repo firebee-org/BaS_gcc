6342196e2bef62383bb81a9f34488e6427d2fcd8
Markus Fröschle <mfro@mubf.de>
2013-08-23 07:29:02 +0000

    added start of pci implementation

---
+++ include/pci.h
@@ -0,0 +1,10 @@
+#ifndef _PCI_H_
+#define _PCI_H_
+
+#define PCI_MEMORY_OFFSET	0x80000000
+#define PCI_MEMORY_SIZE		0x40000000
+#define PCI_IO_OFFSET		0xB0000000
+#define PCI_IO_SIZE			0x10000000
+
+
+#endif /* _PCI_H_ */
