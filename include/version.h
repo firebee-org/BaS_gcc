ea7c1790c9b9abba532e47530e873d9d19dc6563
Markus Fröschle <mfro@mubf.de>
2013-10-21 19:10:34 +0000

    added GNU header

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
  *  Created on: 21.10.2013
  *      Author: mfro
  *
- *       * This file is part of BaS_gcc.
+ * This file is part of BaS_gcc.
  *
  * BaS_gcc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
