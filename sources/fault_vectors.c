d179b26abc166bda7a0dd3a24084656927a9e256
Markus Fröschle <mfro@mubf.de>
2013-10-21 18:34:19 +0000

    added missing GNU header

--- sources/fault_vectors.c
+++ sources/fault_vectors.c
@@ -1,11 +1,26 @@
-#include "MCF5475.h"
-#include "bas_types.h"
-#include "bas_printf.h"
-
 /*
+ * This file is part of BaS_gcc.
+ *
+ * BaS_gcc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BaS_gcc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BaS_gcc.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * provide an early exception vector branch table to catch exceptions _before_ VBR has been setup eventually
  * (to RAMBAR0, in exceptions.S)
  */
+
+#include "MCF5475.h"
+#include "bas_types.h"
+#include "bas_printf.h"
 
 typedef void (*exception_handler)(void);
 extern exception_handler SDRAM_VECTOR_TABLE[];
