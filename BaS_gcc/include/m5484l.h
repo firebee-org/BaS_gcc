6eef95d0d0571aefb10763e3239f9a06a411bdc7
Markus Fröschle <mfro@mubf.de>
2013-11-06 13:18:28 +0000

    fixed formatting

--- BaS_gcc/include/m5484l.h
+++ BaS_gcc/include/m5484l.h
@@ -29,11 +29,11 @@
 
 #define SYSCLK			100000
 
-#define BOOTFLASH_BASE_ADDRESS	0xe0000000
-#define BOOTFLASH_SIZE				0x400000				/* LITEKIT has 4MB flash */
+#define BOOTFLASH_BASE_ADDRESS		0xe0000000
+#define BOOTFLASH_SIZE				0x400000	/* LITEKIT has 4MB flash */
 #define BOOTFLASH_BAM				(BOOTFLASH_SIZE - 1)
 
-#define SDRAM_START					0x00000000  /* start at address 0 */
+#define SDRAM_START					0x00000000	/* start at address 0 */
 #define SDRAM_SIZE					0x4000000  	/* 64 MB on the LITEKIT */
 
 #ifdef COMPILE_RAM
@@ -42,8 +42,8 @@
 #define TARGET_ADDRESS	BOOTFLASH_BASE_ADDRESS
 #endif /* COMPILE_RAM */
 
-#define	USB_BUFFER_SIZE	0x80000
+#define USB_BUFFER_SIZE	0x80000
 
-#define EMUTOS_BASE_ADDRESS 0xe0100000
+#define EMUTOS_BASE_ADDRESS 	0xe0100000
 
 #endif /* _M5484L_H_ */
