3da81e5c7ac6a54622383db4588e2117be939ebb
Markus Fröschle <mfro@mubf.de>
2013-11-19 14:41:05 +0000

    added gnu header

--- BaS_gcc/sources/mmu.c
+++ BaS_gcc/sources/mmu.c
@@ -1,5 +1,28 @@
 #include "mmu.h"
 
+/*
+ * mmu.c
+ *
+ * This file is part of BaS_gcc.
+ *
+ * BaS_gcc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BaS_gcc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BaS_gcc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * derived from original assembler sources:
+ * Copyright 2010 - 2012 F. Aschwanden
+ * Copyright 2013        M. Froeschle
+ */
+
 #define ACR_BA(x)                   ((x) & 0xffff0000)
 #define ACR_ADMSK(x)                (((x) & 0xffff) << 16)
 #define ACR_E(x)                    (((x) & 1) << 15)
