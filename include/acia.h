a96933b54b9afd4c92333b33451d99eba78a60b3
Markus Fröschle <mfro@mubf.de>
2013-12-11 06:26:41 +0000

    added code to access MIDI and IKBD in mmu.c

--- include/acia.h
+++ include/acia.h
@@ -4,9 +4,9 @@
 /*
  * ACIA registers
  */
-#define keyctl	volatile uint8_t *0xfffc00
-#define keybd	volatile uint8_t *0xfffc02
-#define midictl	volatile uint8_t *0xfffc04
-#define midi	volatile uint8_t *0xfffc06
+#define keyctl	0xfffc00
+#define keybd	0xfffc02
+#define midictl	0xfffc04
+#define midi	0xfffc06
 
 #endif /* _ACIA_H_ */
