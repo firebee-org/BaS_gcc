69a4379b4465fcb68334d04850adf6d7ac40a5b8
Markus Fröschle <mfro@mubf.de>
2013-12-22 16:14:53 +0000

    added files to manage uncached driver memory

---
+++ BaS_gcc/include/driver_mem.h
@@ -0,0 +1,12 @@
+#ifndef _DRIVER_MEM_H_
+#define _DRIVER_MEM_H_
+
+/*
+ * the driver_mem module provides a block of _uncached_ memory for USB and other drivers as
+ * well as some memory handling functions for it
+ */
+extern int driver_mem_init(void);
+extern void *driver_mem_alloc(long amount);
+extern int driver_mem_free(void *addr);
+
+#endif /* _DRIVER_MEM_H_ */
