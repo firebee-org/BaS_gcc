1ddd779397ec150b8a19b49e927d0c30b0982bb4
Markus Fröschle <mfro@mubf.de>
2015-11-20 21:25:07 +0000

    new revision of BaS native PCI driver that supports find_pci_device() and find_pci_classcode() functions from TOS

--- BaS_gcc/include/driver_vec.h
+++ BaS_gcc/include/driver_vec.h
@@ -271,7 +271,7 @@ struct mmu_driver_interface
     uint32_t (*report_pagesize)(void);
 };
 
-struct pci_native_driver_interface
+struct pci_native_driver_interface_0_1
 {
     uint32_t (*pci_read_config_longword)(int32_t handle, int offset);
     uint16_t (*pci_read_config_word)(int32_t handle, int offset);
@@ -286,6 +286,22 @@ struct pci_native_driver_interface
     struct pci_rd * (*pci_get_resource)(int32_t handle);
 };
 
+struct pci_native_driver_interface
+{
+    uint32_t (*pci_read_config_longword)(int32_t handle, int offset);
+    uint16_t (*pci_read_config_word)(int32_t handle, int offset);
+    uint8_t (*pci_read_config_byte)(int32_t handle, int offset);
+
+    int32_t (*pci_write_config_longword)(int32_t handle, int offset, uint32_t value);
+    int32_t (*pci_write_config_word)(int32_t handle, int offset, uint16_t value);
+    int32_t (*pci_write_config_byte)(int32_t handle, int offset, uint8_t value);
+    int32_t (*pci_hook_interrupt)(int32_t handle, void *handler, void *parameter);
+    int32_t (*pci_unhook_interrupt)(int32_t handle);
+    int32_t (*pci_find_device)(uint16_t device_id, uint16_t vendor_id, int index);
+    int32_t (*pci_find_classcode)(uint32_t classcode, int index);
+    struct pci_rd * (*pci_get_resource)(int32_t handle);
+};
+
 union interface
 {
     struct generic_driver_interface *gdi;
@@ -294,6 +310,7 @@ union interface
     struct framebuffer_driver_interface *fb;
     struct pci_bios_interface *pci;
     struct mmu_driver_interface *mmu;
+    struct pci_native_driver_interface_0_1 *pci_native_0_1;
     struct pci_native_driver_interface *pci_native;
 };
 
