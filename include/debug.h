601cdc347036202ed31299b324dfe282db86a864
Markus Fröschle <mfro@mubf.de>
2016-10-25 15:35:46 +0000

    add inf()

--- include/debug.h
+++ include/debug.h
@@ -7,6 +7,7 @@
 #define dbg(format, arg...) do {;} while (0)
 #endif /* DEBUG */
 #define err(format, arg...) do { xprintf("ERROR (%s()): " format, __FUNCTION__, ##arg); } while(0)
+#define inf(format, arg...) do { xprintf("" format, ##arg); } while(0)
 
 #endif // DEBUG_H
 
