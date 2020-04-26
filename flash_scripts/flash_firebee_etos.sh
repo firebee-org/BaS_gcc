6b9c8351ca72ed930d3f19b66bd87b9eba482384
Markus Fröschle <mfro@mubf.de>
2020-04-26 10:20:57 +0200

    build and flash EmuTOS
    emutos source directory needs to be installed on the same directory level than
    BaS_gcc, than this script can be directly called to build and flash
    EmuTOS for the Firebee. Convenient if you have to do a git bisect

---
+++ flash_scripts/flash_firebee_etos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd ../../emutos
+make clean firebee -j
+m68k-elf-objcopy --alt-machine-code 4 -Isrec -Oelf32-m68k emutosfb.s19 emutosfb.elf
+mv emutosfb.elf ../BaS_gcc/flash_scripts/emutos-firebee.elf
+popd
+time bdmctrl flash_firebee_etos.bdm /dev/bdmcf3
