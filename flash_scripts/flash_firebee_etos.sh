7e9dfc5f27fa6e3460564117319c0c783720cba3
Markus Fröschle <mfro@mubf.de>
2024-02-07 18:47:35 +0100

    update emutos flash script to new target name

--- flash_scripts/flash_firebee_etos.sh
+++ flash_scripts/flash_firebee_etos.sh
@@ -2,7 +2,7 @@
 
 pushd ../../emutos
 make clean firebee -j
-m68k-elf-objcopy --alt-machine-code 4 -Isrec -Oelf32-m68k emutosfb.s19 emutosfb.elf
+m68k-elf-objcopy --alt-machine-code 4 -Isrec -Oelf32-m68k etosfbus.s19 emutosfb.elf
 mv emutosfb.elf ../BaS_gcc/flash_scripts/emutos-firebee.elf
 popd
 time bdmctrl flash_firebee_etos.bdm /dev/bdmcf3
